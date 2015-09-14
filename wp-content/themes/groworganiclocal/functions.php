8dfd9f966799d38e53f998ef95a180087fb62fb2
DJ Wilkerson <djay.wilk@gmail.com>
2015-09-14 13:10:12 -0500

    Commit 2

--- wp-content/themes/groworganiclocal/functions.php
+++ wp-content/themes/groworganiclocal/functions.php
@@ -4,3 +4,87 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+function create_user_from_registration($cfdata) {
+    if (!isset($cfdata->posted_data) && class_exists('WPCF7_Submission')) {
+        // Contact Form 7 version 3.9 removed $cfdata->posted_data and now
+        // we have to retrieve it from an API
+        $submission = WPCF7_Submission::get_instance();
+        if ($submission) {
+            $formdata = $submission->get_posted_data();
+        }
+    } elseif (isset($cfdata->posted_data)) {
+        // For pre-3.9 versions of Contact Form 7
+        $formdata = $cfdata->posted_data;
+    } else {
+        // We can't retrieve the form data
+        return $cfdata;
+    }
+    // Check this is the user registration form
+    if ( $cfdata->title() == 'User Registration') {
+        $password = wp_generate_password( 12, false );
+        $email = $formdata['email'];
+        $name = $formdata['name'];
+        $role = $formdata['organization'];
+        // Construct a username from the user's name
+        $username = strtolower(str_replace(' ', '', $name));
+        $name_parts = explode(' ',$name);
+        if ( !email_exists( $email ) ) {
+            // Find an unused username
+            $username_tocheck = $username;
+            $i = 1;
+            while ( username_exists( $username_tocheck ) ) {
+                $username_tocheck = $username . $i++;
+            }
+            $username = $username_tocheck;
+            // Create the user
+            $userdata = array(
+                'user_login' => $username,
+                'user_pass' => $password,
+                'user_email' => $email,
+                'nickname' => reset($name_parts),
+                'display_name' => $name,
+                'first_name' => reset($name_parts),
+                'last_name' => end($name_parts),
+                'role' => $role
+            );
+            $user_id = wp_insert_user( $userdata );
+            if ( !is_wp_error($user_id) ) {
+                // Email login details to user
+                $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+                $message = "Welcome! Your login details are as follows:" . "\r\n";
+                $message .= sprintf(__('Username: %s'), $username) . "\r\n";
+                $message .= sprintf(__('Password: %s'), $password) . "\r\n";
+                $message .= wp_login_url() . "\r\n";
+                wp_mail($email, sprintf(__('[%s] Your username and password'), $blogname), $message);
+            }
+        }
+    }
+    return $cfdata;
+}
+add_action('wpcf7_before_send_mail', 'create_user_from_registration', 1);
+
+// RELINK LOG IN LOGO
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+// ADDING CUSTOM STYLING TO THE LOG IN 
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_theme_root_uri() . '/groworganiclocal/css/gol-loginstyle.css' );
+ //   wp_enqueue_script( 'custom-login', get_theme_root_uri() . '/style-login.js' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
+add_filter( 'wp_nav_menu_secondary_items','wpsites_loginout_menu_link' );
+
+function wpsites_loginout_menu_link( $menu ) {
+    $loginout = wp_loginout($_SERVER['REQUEST_URI'], false );
+    $menu .= $loginout;
+    return $menu;
+}
